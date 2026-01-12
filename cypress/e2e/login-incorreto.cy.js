2d35796afe0a9cd8357812f0a377322cb52a5fb6
ricardojaj <rickanjos85@gmail.com>
2026-01-12 17:48:20 -0300

    stubPost para teste de login incorreto

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -2,16 +2,20 @@ describe('Pagina de Login', () => {
     beforeEach(() => {
         cy.visit('https://adopet-frontend-cypress.vercel.app/');
         cy.get('[data-test="login-button"]').click();
-    })
-
-  it('Deve preencher os campos do login corretamente e autenticar o usuario na pagina', () => {
-    cy.get('[data-test="input-loginEmail"]').type('MarcelaTeste123');
-    cy.get('[data-test="input-loginPassword"]').type('123');
-    cy.get('[data-test="submit-button"]').click();
-
-    cy.contains('Por favor, verifique o email digitado').should('be.visible')
-    cy.contains('A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres').should('be.visible')
+        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+          statusCode: 400, }).as('stubPost')
+        })
     
 
+  it('Deve preencher os campos do login incorretamente e verificar erro ao autenticar o usuario na pagina', () => {
+    cy.get('[data-test="submit-button"]').click();
+
+    cy.contains('É necessário informar um endereço de email').should('be.visible');
+    cy.contains('Insira sua senha').should('be.visible');
+  })
+  it('Deve falhar mesmo que os campos sejam preenchidos corretamente', ()=> {
+    cy.login('ricardo@email.com', 'Senha123');
+    cy.wait('@stubPost');
+    cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible');
   })
 })
